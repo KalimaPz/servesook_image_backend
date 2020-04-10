8f672822664115312a8bae39a695257332089733
Donnukrit Satirakul <kalima007c@gmail.com>
2020-04-10 14:04:30 +0700

    Update methods fully working
    reduced code :D add more debug text and more comment , fix id not show when update

--- index.js
+++ index.js
@@ -37,27 +37,30 @@ router
 
     if (persons.length > 0) person.id = +persons[persons.length - 1].id + 1;
     else person.id = 0;
-    console.log(req.body)
-    
+    // console.log('req body')
+    // console.log(req.body)
     person = {
       id: person.id,
-      first_name : req.body.first_name,
-      last_name : req.body.last_name,
-      nick_name : req.body.nick_name,
-      tel : req.body.tel,
-      email : req.body.email,
-      university : req.body.university,
-      address : {
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      nick_name: req.body.nick_name,
+      tel: req.body.tel,
+      email: req.body.email,
+      university: req.body.university,
+      address: {
         home_id: req.body.address.home_id,
         building: req.body.address.building,
         street: req.body.address.street,
         district: req.body.address.district,
         province: req.body.address.province,
         zip_code: req.body.address.zip_code,
-      }
-    }
+      },
+    };
+    // console.log('person data')
     // console.log(person)
+    // console.log('--- post complete --- ')
     persons.push(person);
+    console.log("a new person has been created");
     res.json({ message: "a new person has been created" });
   });
 router
@@ -68,21 +71,41 @@ router
     res.json(persons[index]);
   })
   .put((req, res) => {
-    let id = req.param.person_id;
-    let index = persons.findIndex((persons) => person.id === +id);
+    let id = req.params.person_id;
+    let index = persons.findIndex((person) => person.id === +id);
+    
+    // console.log('params',req.params)
+    // console.log('body',req.body)
 
-    persons[index].first_name = req.body.first_name;
-    persons[index].last_name = req.body.last_name;
-    persons[index].tel = req.body.tel;
-    persons[index].email = req.body.email;
-    persons[index].university = req.body.university;
-    // persons[index].location = req.body.location
+    persons[index] = {
+      id: +id,
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      nick_name: req.body.nick_name,
+      tel: req.body.tel,
+      email: req.body.email,
+      university: req.body.university,
+      address: {
+        home_id: req.body.address.home_id,
+        building: req.body.address.building,
+        street: req.body.address.street,
+        district: req.body.address.district,
+        province: req.body.address.province,
+        zip_code: req.body.address.zip_code,
+      },
+    };
+    // console.log('--- show updated data ---')
+    // console.log(persons[index])
+    // console.log('--- put complete --- ')
+    console.log("this person has been deleted");
+    res.json({ message: "this person has been updated" });
   })
   .delete((req, res) => {
     let id = req.param.person_id;
     let index = persons.findIndex((person) => person.id === +id);
     persons.slice(index, 1);
-    res.json({ message: "a person has been deleted" });
+    console.log("this person has been deleted");
+    res.json({ message: "this person has been deleted" });
   });
 app.use("*", (req, res) => res.status(404).send("404 Not found"));
 app.listen(PORT, () => console.log("Server is Running ON : ", PORT));
