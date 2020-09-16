2938c136bb590e1f8632bc591e301d72426cbd74
KalimaPz <kalima007c@gmail.com>
2020-09-16 14:36:08 +0700

    resize < 100kb

--- index.js
+++ index.js
@@ -1,28 +1,49 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const url =
-  "https://passport.psu.ac.th/authentication/authentication.asmx?wsdl";
 const app = express();
 const cors = require("cors");
 const PORT = process.env.PORT || 8080
 
-let image = {
-  "imageUri" : [
-    'https://th-live-01.slatic.net/p/bbc847afac4930608b2f2e9b06ae1519.jpg',
-    'https://s3.amazonaws.com/finegardening.s3.tauntoncloud.com/app/uploads/vg-migration/2018/11/28004024/sage.JPG',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcScYqBgmWO9Jgsv7WbImqc5mqLQFYM_l8XDew&usqp=CAU',
-    'https://images.homedepot-static.com/productImages/11b2e243-983f-4750-ae86-d8ca8f62edcf/svn/gurney-s-vegetable-herb-seeds-14592-64_1000.jpg',
-    'https://www.prairieviewfarmmarket.com/wp-content/uploads/2016/03/taragon.jpg',
-  ],
-  "source" :"CCTV"
+let image = [
+  {
+    "id" : 0,
+    "key" : 'now',
+    "link" : '',
+  },
+  {
+    "id" : 1,
+    "key" : 'today',
+    "link" : '',
+  },
+  {
+    "id" : 2,
+    "key" : 'yesterday',
+    "link" : '',
+  },
+  {
+    "id" : 3,
+    "key" : 'two_days_ago',
+    "link" : '',
+  },
+]
+
+let imageUri = {
+  "imageUri ": [
+    'https://firebasestorage.googleapis.com/v0/b/servesook-farm-alpha.appspot.com/o/768px-Round_Landmark_School_Icon_-_Transparent.svg.png?alt=media&token=7b65d4bc-abf2-496f-933d-1f3c1cd38fa1',
+    'https://firebasestorage.googleapis.com/v0/b/servesook-farm-alpha.appspot.com/o/768px-Round_Landmark_School_Icon_-_Transparent.svg.png?alt=media&token=7b65d4bc-abf2-496f-933d-1f3c1cd38fa1',
+    'https://firebasestorage.googleapis.com/v0/b/servesook-farm-alpha.appspot.com/o/768px-Round_Landmark_School_Icon_-_Transparent.svg.png?alt=media&token=7b65d4bc-abf2-496f-933d-1f3c1cd38fa1',
+    'https://firebasestorage.googleapis.com/v0/b/servesook-farm-alpha.appspot.com/o/768px-Round_Landmark_School_Icon_-_Transparent.svg.png?alt=media&token=7b65d4bc-abf2-496f-933d-1f3c1cd38fa1',
+  ] ,
+  "source" : "CCTV"
 }
+
 
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.get("/", (req, res) => {
-  return res.json(image)
+  return res.json(imageUri)
 });
 app.listen(PORT, () => console.log("SERVER IS RUNNING ON ", PORT));
